c65dab0e52f4ccfe70877d6517de39862cdc95d5
Nathan <nathan.arriesgado0g@gmail.com>
2023-04-23 03:19:30 +0800

    [Feat][Nathan] Created route to payment options page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/payment/options', function () {
+    return view('payment_options');
+});
