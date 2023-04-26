2071cdc7bfa1d084e853b4baa7b474fb83598f25
frankie1015 <francineysabelle2@gmail.com>
2023-04-26 14:28:12 +0800

    frature: login implementation

---
+++ resources/js/components/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-table striped hover :items="items" :fields = "fields"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: null,
+        fields: ['id', 'USERNAME']
+      }
+    },
+    methods:{
+      get_accounts(){
+        axios.get('/get_accounts').then(response=>{
+          console.log(response.data);
+          this.items = response.data;
+        }).catch(error=>{
+          console.log(error.data);
+        })
+      }
+    },
+    beforeMount(){
+      this.get_accounts();
+    }
+  }
+</script>
