38958929cdb609dd9e9b3eac72f544dfc8dad8d6
joningning <trinity21798@gmail.com>
2023-04-29 17:36:25 +0800

    improved page for marketing

--- resources/views/marketingAutomation.blade.php
+++ resources/views/marketingAutomation.blade.php
@@ -21,6 +21,7 @@
     </head>
     <body class="antialiased">
         <div id="app">
+            <marketing-automation-component></marketing-automation-component>
         </div>
         <script src="https://apis.google.com/js/platform.js" async defer></script>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
