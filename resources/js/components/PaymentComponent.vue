a68f4f9cc5d98632d6666b56c11d5f637519d384
keylperez <perez.williamcaleb@gmail.com>
2023-04-29 12:57:09 +0800

    resolved conflicts

--- resources/js/components/PaymentComponent.vue
+++ resources/js/components/PaymentComponent.vue
@@ -1,21 +1,25 @@
 <template>
     <div :class="['payment-option', paymentMethod]" @click="goToGcash">
-      <img :id="paymentMethod" :src="logoUrl" :alt="paymentMethod + ' Logo'">
+        <img
+            :id="paymentMethod"
+            :src="logoUrl"
+            :alt="paymentMethod + ' Logo'"
+        />
     </div>
 </template>
 
 <style>
-  body {
+body {
     margin: 0;
     body: 0;
-  }
+}
 
-  .payment-container {
+.payment-container {
     display: flex;
     height: 100vh;
-  }
+}
 
-  .payment-option {
+.payment-option {
     flex: 1;
     display: flex;
     justify-content: center;
@@ -24,84 +28,83 @@
     transition: background-color 0.2s;
     height: 100%;
     width: 100%;
-  }
+}
 
-  .payment-option:hover {
+.payment-option:hover {
     filter: brightness(80%);
-  }
+}
 
-  .GCash {
+.GCash {
     background-color: rgb(0, 125, 254);
-  }
+}
 
-  .PayMaya {
+.PayMaya {
     background-color: rgb(29, 45, 99);
-  }
+}
 
-  .PayPal {
-    background-color: #FFFFFF;
-  }
+.PayPal {
+    background-color: #ffffff;
+}
 
-  img {
+img {
     width: 20vw;
     height: auto;
     pointer-events: none;
-  }
+}
 
-  #GCash {
+#GCash {
     width: 28vw;
-  }
+}
 
-  #PayMaya {
+#PayMaya {
     width: 32vw;
-  }
+}
 
-  #PayPal {
+#PayPal {
     width: 24vw;
-  }
-
+}
 </style>
 
 <script>
-  import gcashLogo from '@/../../public/images/gcash_logo.png';
-  import paymayaLogo from '@/../../public/images/paymaya_logo.png';
-  import paypalLogo from '@/../../public/images/paypal_logo.png';
+import gcashLogo from "@/../../public/img/gcash_logo.png";
+import paymayaLogo from "@/../../public/img/paymaya_logo.png";
+import paypalLogo from "@/../../public/img/paypal_logo.png";
 
-  export default {
+export default {
     props: {
-      paymentMethod: {
-        type: String,
-        required: true,
-      }
+        paymentMethod: {
+            type: String,
+            required: true,
+        },
     },
     computed: {
-      logoUrl() {
-        switch (this.paymentMethod) {
-          case 'GCash':
-            return gcashLogo;
-          case 'PayMaya':
-            return paymayaLogo;
-          case 'PayPal':
-            return paypalLogo;
-          default:
-            return '';
-        }
-      },
+        logoUrl() {
+            switch (this.paymentMethod) {
+                case "GCash":
+                    return gcashLogo;
+                case "PayMaya":
+                    return paymayaLogo;
+                case "PayPal":
+                    return paypalLogo;
+                default:
+                    return "";
+            }
+        },
     },
     methods: {
-      goToPaymentPage() {
-        switch (this.paymentMethod) {
-          case 'gcash':
-            this.$router.push('/');
-            break;
-          case 'paymaya':
-            this.$router.push('/');
-            break;
-          case 'paypal':
-            this.$router.push('/');
-            break;
-        }
-      }
-    }
-  };
+        goToPaymentPage() {
+            switch (this.paymentMethod) {
+                case "gcash":
+                    this.$router.push("/");
+                    break;
+                case "paymaya":
+                    this.$router.push("/");
+                    break;
+                case "paypal":
+                    this.$router.push("/");
+                    break;
+            }
+        },
+    },
+};
 </script>
