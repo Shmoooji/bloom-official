28248e770869bcc1b3bf933b7d35fd8ddea4646b
JoshuaCT-coder <rodriguezjoshuausc@gmail.com>
2023-05-06 21:36:24 +0800

    R1 - trial and error

---
+++ resources/js/pages/analyticsGraphs.vue
@@ -0,0 +1,215 @@
+
+<style>
+.content-body {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+.content-body analytics-sidebar {
+    display: flex;
+    justify-content: left;
+}
+
+h3 {
+    text-align: center;
+}
+
+.head-title {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+
+.head-title h1 {
+    margin: 0 auto 0 auto;
+    text-align: center;
+    flex-grow: 1;
+}
+.head-title h4 {
+    text-align: center;
+    flex-grow: 1;
+    color: white;
+}
+
+.content-main {
+  position: absolute;
+  top: 45%;
+  left: 55%;
+  transform: translate(-50%, -50%);
+  border: solid 2px #4b4237;
+  border-radius: 20px;
+  padding: 20px;
+  padding-bottom: 40px;
+  min-width: 1300px;
+  max-width: 80%;
+  width: auto;
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+.content-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 18px;
+  width: 100%;
+  table-layout: fixed;
+  font-weight: bold;
+  margin: auto;
+}
+
+/* Adjustments for smaller screens */
+@media only screen and (max-width: 1200px) {
+  .content-main {
+    top: 45%;
+    left: 55%;
+    transform: translate(-50%, -50%);
+    border-radius: 10px;
+    padding: 10px;
+    padding-bottom: 20px;
+    min-width:80%;
+    max-width: 600px;
+    font-size: 14px;
+  }
+
+  .content-table {
+    margin: 10px 0;
+    font-size: 16px;
+  }
+}
+
+/* Adjustments for even smaller screens */
+@media only screen and (max-width: 480px) {
+  .content-main {
+    top: 60%;
+    left:60%;
+    transform: translate(-50%, -50%);
+    border-radius: 5px;
+    padding: 5px;
+    padding-bottom: 10px;
+    min-width: 80%;
+    max-width: 80%;
+    font-size: 12px;
+  }
+
+  .content-table {
+    margin: 5px 0;
+    font-size: 14px;
+  }
+}
+
+.content-table thead tr {
+    background-color: #b3b3b3;
+    color: #4b4237;
+    text-align: center;
+    border-radius: 100px;
+    border: solid 1px #736b60;
+}
+
+
+.content-table th,
+td {
+    padding: 12px 15px;
+    text-align: center;
+}
+
+/* Inactive row hover */
+.content-table tbody tr:hover {
+    background-color: rgba(94, 80, 80, 0.05);
+    cursor:not-allowed;
+}
+
+/* Active row hover */
+.content-table tbody tr:hover td {
+    background-color: rgba(77, 70, 70, 0.1);
+}
+
+
+
+div {
+    font-family: "Jost", sans-serif !important;
+}
+
+.navbar{
+    width: 100vw;
+}
+
+</style>
+
+
+
+<template>
+
+    <div class="analytics-graphs">
+        <div>
+            <nav-bar></nav-bar>
+        </div>
+        <b-row>
+            <b-col><analytics-side-bar></analytics-side-bar></b-col>
+                
+            <b-col>
+                <div class="content-main">
+        <div class="head-title">
+            <h1>Tabluar Data</h1>
+            <h4>(dummy data)</h4>
+        </div>
+        <table class="content-table">
+            <thead>
+                <tr>
+                    <th>Campaigns</th>
+                    <th>Sales for Campaigns</th>
+                    <th>Sources</th>
+                    <th>Customer satisfaction</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr class="table-row">
+                    <td></td>
+                    <td>(Arranged in Descending order)</td>
+                    <td>(Arranged by Count)</td>
+                    <td>(Arranged by Average Rating)</td>
+                </tr>
+                <tr class="table-row">
+                    <td>Campaign 1</td>
+                    <td>Php 15,000</td>
+                    <td>Adverts</td>
+                    <td>4.0</td>
+                </tr>
+                <tr class="table-row" >
+                    <td>Campaign 2</td>
+                    <td>Php 12,050</td>
+                    <td>Emails & Sponsors</td>
+                    <td>3.8</td>
+                </tr>
+                <tr class="table-row">
+                    <td>Campaign 3</td>
+                    <td>Php 11,100</td>
+                    <td>Others</td>
+                    <td>3.7</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+            </b-col>
+        </b-row>
+            
+   
+</div>
+</template>
+<script>
+
+export default {
+    components: {AnalyticsSideBar, NavBar},
+    name: "Analytics Graphs",
+    mounted() {
+        console.log("Analytics Graphs Shown in the Screen");
+    },
+};
+
+import AnalyticsSideBar from '../components/analyticsSideBar.vue';
+import NavBar from '../components/NavBar.vue';
+</script>
+
+
---
+++ resources/views/graphs.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Scripts -->
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <style>
+        body {
+            height: 100vh;
+            display: flex;
+            background: linear-gradient(to top left, #28b487, #7dd56f);
+        }
+
+        h1 {
+            font-family: sans-serif;
+            font-size: 50px;
+            line-height: 1.3;
+            width: 100%;
+            padding: 30px;
+            text-align: center;
+            color: white;
+        }
+
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+
+        #app {
+            margin-top: -3vh;
+        }
+
+        .graph {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+
+<body class="antialiased">
+    <div id="app">
+        <analytics-navbar></analytics-navbar>
+        <div class="content-body">
+            <div class="sidebar">
+                <analytics-sidebar></analytics-sidebar>
+            </div>
+            <div class="graph">
+                <analytics-graphs></analytics-graphs>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
