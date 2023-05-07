ed72e7cabb63bbedbcf3f6c4324d49e30bc06bf7
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-07 16:27:02 +0800

    [Feat][Nathan] Created new view for successful payments

---
+++ resources/views/payment_success.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <h1>Payment Succesful</h1>
+        <h1>Payment ID: {{ $paymentID }} </h1>
+        <h1>Payer ID: {{ $payerID }} </h1>
+    </body>
+</html>
