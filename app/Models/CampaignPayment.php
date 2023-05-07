18331189caec360df74987932e5e0fa41d6003bc
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-07 16:29:58 +0800

    [Feat][Nathan] Created savePaymentInfo() method for saving payments to database with temporary dummy data

--- app/Models/CampaignPayment.php
+++ app/Models/CampaignPayment.php
@@ -5,7 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use DB;
+
 class CampaignPayment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 
+                           'campaign_type', 
+                           'payment_method', 
+                           'subscription_period'];
+
+    public static function savePaymentInfo(array $info) {
+        DB::table('campaign_payments')->insert([
+            'user_id' => $info['user_id'],
+            'campaign_type' => $info['campaign_type'],
+            'payment_method' => $info['payment_method'],
+            'subscription_period' => $info['subscription_period']
+        ]);
+    }
 }
