74f0863a71d540a14de593271923ab66b6bf3b44
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-07 14:57:13 +0800

    [Feat][Nathan] Added PaymentController with functions displayOptions() and createPaymentPaypal()

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Omnipay\Omnipay;
+
+class PaymentController extends Controller
+{
+    private $gateway;
+
+    public function __construct() {
+        $this->gateway = Omnipay::create('PayPal_Rest');
+        $this->gateway->setClientId(env('PAYPAL_CLIENT_ID'));
+        $this->gateway->setSecret(env('PAYPAL_SECRET'));
+        $this->gateway->setTestMode(true);
+    }
+
+    public function displayOptions() {
+        return view('payment_options');
+    }
+
+    public function createPaymentPaypal(Request $request) {
+        try {
+           
+            $response = $this->gateway->purchase(array(
+                'amount' => $request->amount,
+                'currency' => env('PAYPAL_CURRENCY'),
+                'returnUrl' => url('/payment/success'),
+                'cancelUrl' => url('/payment/error'),
+
+            ))->send();
+
+            if ($response->isRedirect()) {
+                $response->redirect();
+            } else {
+                return $response->getMessage();
+            }
+
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+}
