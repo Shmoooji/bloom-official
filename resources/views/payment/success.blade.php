68537d160b7c3ccb10b03851d5990fb149887531
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-10 02:28:38 +0800

    [Fix][Nathan] Segregated payment views

--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -4,7 +4,7 @@
     </head>
 
     <body>
-        <h1>Payment Succesful</h1>
+        <h1>Payment Succesful</h1> 
         <h1>Payment ID: {{ $paymentID }} </h1>
         <h1>Payer ID: {{ $payerID }} </h1>
     </body>
