967981f966379698ce58133d39be62b767055695
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-10 02:25:22 +0800

    [Feat][Nathan] Added paymentIsUnique() method to check if payment_id has been previously submitted

--- app/Models/CampaignPayment.php
+++ app/Models/CampaignPayment.php
@@ -16,8 +16,16 @@ class CampaignPayment extends Model
                            'payment_method', 
                            'subscription_period',
                            'payment_id'];
+                           
+    public static function paymentIsUnique($paymentID) {
+        $existingPayment = DB::table('campaign_payments')
+                             ->where('payment_id', $paymentID)
+                             ->first();
 
-    public static function savePaymentInfo(array $info) {
+        return is_null($existingPayment);
+    }
+
+    public static function savePaypalInfo(array $info) {
         DB::table('campaign_payments')->insert([
             'user_id' => $info['user_id'],
             'campaign_type' => $info['campaign_type'],
