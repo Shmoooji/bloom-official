a020b46c914cac7318f78bd5ec26e60cc56aa626
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-11 05:27:19 +0800

    [Misc][Nathan] Renamed payment model to reflect previous changes

--- app/Models/CampaignPayment.php
+++ app/Models/CampaignPayment.php
@@ -16,19 +16,11 @@ class CampaignPayment extends Model
                            'payment_method', 
                            'subscription_period',
                            'payment_id'];
-                           
-    public static function paymentIsUnique($paymentID) {
-        $existingPayment = DB::table('campaign_payments')
-                             ->where('payment_id', $paymentID)
-                             ->first();
-
-        return is_null($existingPayment);
-    }
-
+                        
     public static function savePaypalInfo(array $info) {
         DB::table('campaign_payments')->insert([
             'user_id' => $info['user_id'],
-            'campaign_type' => $info['campaign_type'],
+            'campaign_id' => $info['campaign_type'],
             'payment_method' => $info['payment_method'],
             'subscription_period' => $info['subscription_period'],
             'payment_id' => $info['payment_id'],
