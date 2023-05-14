b8397516113490378c2c13dc121bab0487743940
Hop-D <95290625+Hop-D@users.noreply.github.com>
2023-05-14 19:38:07 +0800

    Create footer.vue

---
+++ resources/js/components/footer.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="footer">
+        <footer>
+            <div class="row">
+                    <div class="col-md-12 text-center">
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <a href="#">Privacy Policy</a>
+                            </li>
+                            <li class="list-inline-item">|</li>
+                            <li class="list-inline-item">
+                                <a href="#">Safe Guarding Statement</a>
+                            </li>
+                            <li class="list-inline-item">|</li>
+                            <li class="list-inline-item">
+                                <a href="/about">About Us</a>
+                            </li>
+                            <li class="list-inline-item">|</li>
+                            <li class="list-inline-item">
+                                <a href="/contactUs">Contact Us</a>
+                            </li>
+                        </ul>
+                        <p class="mb-0 mt-3 footer-p">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Proin sollicitudin, est quis porta consequat,
+                            sem nisi pulvinar lorem, a t ornare lorem eros id
+                            sapien. Ut enim lacus, egestas eu egestas et, auctor
+                            eget libero. Quisque et urna velit. Maecenas est
+                            arcu, dapibus ut risus in, posuere tempus arcu.
+                        </p>
+                    </div>
+                </div>
+        </footer>
+    </div>
+
+</template>
+
+<script>
+	export default {
+
+    name: "Footer",
+    };
+</script>
+
+
+<style>
+    .footer {
+        background-color: #86A760;
+        height: 11em;
+        padding: 2em;
+    }
+
+    .list-inline-item,
+    .list-inline-item a,
+    .footer-p {
+        color: #242108;
+    }
+
+</style>
