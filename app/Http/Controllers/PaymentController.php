97ca772d5174e18c6f87d0157738ce045e42ef0c
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-16 18:53:01 +0800

    [Fix][Nathan] Added abort procedures for unsuccessful requests

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -10,18 +10,21 @@ class PaymentController extends Controller
 {
     private $gateway;
 
-    public function __construct() {
+    public function __construct() 
+    {
         $this->gateway = Omnipay::create('PayPal_Rest');
         $this->gateway->setClientId(env('PAYPAL_CLIENT_ID'));
         $this->gateway->setSecret(env('PAYPAL_SECRET'));
         $this->gateway->setTestMode(true);
     }
 
-    public function index() {
+    public function index() 
+    {
         return view('payment.options');
     }
 
-    public function createPaymentPaypal(Request $request) {
+    public function createPaymentPaypal(Request $request) 
+    {
         try 
         {
             $response = $this->gateway->purchase(array(
@@ -45,53 +48,52 @@ class PaymentController extends Controller
         }
     }
 
-    public function handleSuccess(Request $request) {
-        try 
-        {
-            $PayerID = $request->query('PayerID');
-            $PaymentID = $request->query('paymentId');
-    
-            if (!$PayerID || !$PaymentID) {
-                return redirect()->route('payment/option');
-            }
-    
-            $transaction = $this->gateway->completePurchase(array(
-                'payer_id'             => $PayerID,
-                'transactionReference' => $PaymentID,
-            ));
-    
-            $response = $transaction->send();
-    
-            if (!$response->isSuccessful()) {
-                return redirect()->route('payment/option');
-            }
-    
-            $info = array_merge($request->all(), array(
-                'user_id'             => 4,
-                'campaign_type'       => 3,
-                'payment_method'      => 'PayPal',
-                'subscription_period' => 6,
-                'payment_id'          => $PaymentID
-            ));
-    
-            if (!CampaignPayment::paymentIsUnique($info['payment_id'])) {
-                return redirect()->route('payment/option');
-            }
+    public function handleSuccess(Request $request) 
+    {
+        
+        $PayerID = $request->query('PayerID');
+        $PaymentID = $request->query('paymentId');
 
-            CampaignPayment::savePaypalInfo($info);
-    
-            return view('payment.success')
-                 ->with('paymentID', $PaymentID)
-                 ->with('payerID', $PayerID);
+        if (!$PayerID || !$PaymentID) {
+            return abort(400);
         }
-        catch (Throwable $th)
-        {
-            $th->getMessage();
+
+        $transaction = $this->gateway->completePurchase(array(
+            'payer_id'             => $PayerID,
+            'transactionReference' => $PaymentID,
+        ));
+
+        $response = $transaction->send();
+
+        if (!$response->isSuccessful()) {
+            return abort(400);
+        }
+
+        $info = array_merge($request->all(), array(
+            'user_id'             => 4,
+            'campaign_type'       => 3,
+            'payment_method'      => 'PayPal',
+            'subscription_period' => 6,
+            'payment_id'          => $PaymentID
+        ));
+
+        if (!CampaignPayment::paymentIsUnique($info['payment_id'])) {
+            return redirect()->route('payment/option');
+        }
+
+        try {
+            CampaignPayment::savePaypalInfo($info);
+        } catch (\Throwable $th) {
+            return abort(500);
         }
         
+        return view('payment.success')
+                ->with('paymentID', $PaymentID)
+                ->with('payerID', $PayerID);
     }
 
-    public function handleError() {
+    public function handleError() 
+    {
         return view('payment.error');
     }
 }
