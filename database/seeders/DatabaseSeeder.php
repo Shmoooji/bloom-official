a5340d7b4a6ca901e3e4293ccc7019135073ba46
Hop-D <95290625+Hop-D@users.noreply.github.com>
2023-05-17 13:32:15 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,5 +22,33 @@ class DatabaseSeeder extends Seeder
         //     'l_name' => 'Test Last Name',
         //     'email' => 'test@example.com',
         // ]);
+        \App\Models\Contact::factory(10)->create();
+
+        DB::table('campaigns')->insert([
+            [
+                'type' => 'Basic',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                'price' => '4.99',
+            ],
+            [
+                'type' => 'Exclusive',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                'price' => '19.99',
+            ],
+            [
+                'type' => 'Pro',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                'price' => '49.99',
+            ],
+            [
+                'type' => 'VIP',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                'price' => '99.99',
+            ],
+        ]);
+
+
+        \App\Models\CampaignPayment::factory(10)->create();
+        \App\Models\Deal::factory(10)->create();
     }
 }
