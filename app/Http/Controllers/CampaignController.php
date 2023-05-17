41f34f98fab9719d1d0f393dfe0800d5e5fea330
Hop-D <95290625+Hop-D@users.noreply.github.com>
2023-05-17 13:38:43 +0800

    Update CampaignController.php

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -10,4 +10,16 @@ class CampaignController extends Controller
     protected function index(){
         return view('campaigns');
     }
+
+    public function getCampaigns() {
+        $data = Campaign::all();
+
+        return response()->json($data);
+    }
+
+    public function get_campaigns() {
+        $campaigns = new Campaign();
+        return $campaigns->get_campaigns();
+    }
+
 }
