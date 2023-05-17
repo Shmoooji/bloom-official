22e4b1352e89f13e5ddce9650192ceae7e1b1576
Hop-D <95290625+Hop-D@users.noreply.github.com>
2023-05-17 18:29:16 +0800

    Update campaignCard.vue

--- resources/js/components/campaignCard.vue
+++ resources/js/components/campaignCard.vue
@@ -5,7 +5,7 @@
             <div v-for="campaign in campaigns" :key="campaign.id" class="col-md-6 my-3">
             <b-card class="px-3 py-3 c-card"> <h2><strong>{{ campaign.type }}</strong></h2>
             <b-card-text class="my-5 c-text">{{ campaign.description }}</b-card-text>
-            <b-button href="/payment"  class="c-button" >Subscribe</b-button>
+            <b-button @click="sendCampaign(campaign)"  class="c-button" >Subscribe</b-button>
             </b-card>
         </div>
         </div>
@@ -17,17 +17,6 @@
 	export default {
 
         name: "CampaignCard",
-
-    	// data(){
-    	// 	return {
-        //         campaigns: [
-        //             {id: 1, name: "Basic", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", price: 4.99},
-        //             {id: 2, name: "Pro", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", price: 19.99},
-        //             {id: 3, name: "Exclusive", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", price: 49.99},
-        //             {id: 4, name: "VIP", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", price: 99.99},
-        //         ]
-    	// 	}
-    	// },
 
         data() {
             return {
@@ -50,7 +39,18 @@
                 .catch(error => {
                     console.error(error);
                 });
+            },
+            
+             sendCampaign(campaign) {
+                const myObj = {id: campaign.id, type: campaign.type, price: campaign.price};
+
+                localStorage.setItem('myObj', JSON.stringify(myObj));
+
+                console.log(myObj);
+
+                window.location.href = '/payment';
             }
+            
         }
     };
 
