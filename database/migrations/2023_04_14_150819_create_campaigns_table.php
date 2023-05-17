fee1f28454f19858412e0014425e05a09e6f989f
Hop-D <95290625+Hop-D@users.noreply.github.com>
2023-05-17 13:37:06 +0800

    Update 2023_04_14_150819_create_campaigns_table.php

--- database/migrations/2023_04_14_150819_create_campaigns_table.php
+++ database/migrations/2023_04_14_150819_create_campaigns_table.php
@@ -15,10 +15,8 @@ return new class extends Migration
     {
         Schema::create('campaigns', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id', 'fk_campaigns_user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->string('type');
+            $table->unsignedFloat('price');
         });
     }
 
