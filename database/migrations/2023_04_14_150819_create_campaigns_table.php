d4f0362b023068ccf331b477385fe080efed868d
Hop-D <95290625+Hop-D@users.noreply.github.com>
2023-05-17 21:59:36 +0800

    Update 2023_04_14_150819_create_campaigns_table.php

--- database/migrations/2023_04_14_150819_create_campaigns_table.php
+++ database/migrations/2023_04_14_150819_create_campaigns_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('campaigns', function (Blueprint $table) {
             $table->id();
             $table->string('type');
+            $table->string('description');
             $table->unsignedFloat('price');
         });
     }
